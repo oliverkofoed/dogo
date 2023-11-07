deba36dc9033798b112b516c3fd1649cad2612f1
Oliver <oliver@oliverkofoed.com>
2023-11-07 11:47:53 +0100

    update server to work again

--- registry/resources/server/server.go
+++ registry/resources/server/server.go
@@ -3,21 +3,54 @@ package server
 import (
 	"time"
 
+	"github.com/oliverkofoed/dogo/commandtree"
 	"github.com/oliverkofoed/dogo/schema"
 	"github.com/oliverkofoed/dogo/ssh"
 )
 
+type ServerGroup struct {
+}
+
+type Server struct {
+	Name          string
+	PublicIP      schema.Template `required:"true" defaultdescription:"The public ip to connect to the server with"`
+	SSHPrivateKey schema.Template `required:"true" defaultdescription:"The private key used to authenticate against to the server"`
+	SSHPublicKey  schema.Template `required:"true" defaultdescription:"The public key used to authenticate "`
+	info          *machineInfo
+}
+
+type machineInfo struct {
+	PublicIPs  []string
+	PrivateIPs []string
+}
+
+func (s *Server) OpenConnection() (schema.ServerConnection, error) {
+	privateKey, err := s.SSHPrivateKey.RenderFileBytes(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ip, err := s.PublicIP.Render(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.NewSSHConnection(ip, 22, "root", "", privateKey, time.Second*30)
+}
+
+func (g *ServerGroup) Validate() error {
+	return nil
+}
+
 // Manager is the main entry point for this resource type
 var Manager = schema.ResourceManager{
-	Name:              "server",
-	ResourcePrototype: &server{},
-	GroupPrototype:    &server{},
-}
-
-type server struct {
-	Address string `required:"true" description:"the address of the server to SSH to."`
-}
-
-func (s *server) OpenConnection() (schema.ServerConnection, error) {
-	return ssh.NewSSHConnection(s.Address, 22, "root", "", nil, time.Second*30)
+	Name:              "servers",
+	ResourcePrototype: &Server{},
+	GroupPrototype:    &ServerGroup{},
+	Provision: func(group interface{}, resource interface{}, l schema.Logger) error {
+		return nil
+	},
+	FindUnused: func(shouldExist map[interface{}][]string, decommisionRoot *commandtree.Command, l schema.Logger) ([]string, error) {
+		return []string{}, nil
+	},
 }
